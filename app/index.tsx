ee7479ac9c8969f89fab036aab2937224ea8bf32
Felipe Jung <felipemattoseu@gmail.com>
2025-04-12 11:40:41 -0400

    Enhance Metro configuration for SVG support and add type declarations for PNG and SVG files. Update package dependencies to include react-native-svg-transformer and integrate SVG logo into the app.

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,12 @@ import { Text } from "@/components/ui/text";
 import { HugeiconsIcon } from "@hugeicons/react-native";
 import { SearchIcon } from "@hugeicons/core-free-icons";
 
+import Logo from "@/assets/images/logo.svg";
+
 export default function Index() {
   return (
     <Box className="bg-custom-shape-white flex-1 items-center justify-center">
+      <Logo />
       <Text className="font-poppins text-3xl">Sign In</Text>
       <HugeiconsIcon icon={SearchIcon} />
     </Box>
