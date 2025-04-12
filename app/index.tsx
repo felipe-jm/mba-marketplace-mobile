dae28e3beaf400b7013aa38c6fc366f3b0432671
Felipe Jung <felipemattoseu@gmail.com>
2025-04-12 11:34:27 -0400

    Add HugeIcons dependencies and update TypeScript configuration for module resolution

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
+import { HugeiconsIcon } from "@hugeicons/react-native";
+import { SearchIcon } from "@hugeicons/core-free-icons";
 
 export default function Index() {
   return (
     <Box className="bg-custom-shape-white flex-1 items-center justify-center">
       <Text className="font-poppins text-3xl">Sign In</Text>
+      <HugeiconsIcon icon={SearchIcon} />
     </Box>
   );
 }
